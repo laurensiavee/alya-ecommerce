c05d759c20362e304961fd2c77db5432133e75e1
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-08-07 16:43:40 +0000

    Layout login

--- src/app/component/Label.tsx
+++ src/app/component/Label.tsx
@@ -1,6 +1,6 @@
 export default function Label(props) {
     return (
-        <h3 className="text-sm font-semibold text-l-text">{props.children}</h3>
+        <h3 className="text-sm font-semibold text-l-text-secondary">{props.children}</h3>
     );
   }
   
--- src/app/component/layout/Navbar.tsx
+++ src/app/component/layout/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <nav className="px-5 py-3 bg-gradient-to-r from-l-primary to-l-secondary text-d-text font-semibold flex justify-between">
         <Link href={`/`} className="flex p-1">
           <img src="{ glassesIcon }" className="my-auto w-5 me-2"></img>
-          Megane
+          Alya
         </Link>
         <Link href={`/`} className="flex p-1">
           Logout
