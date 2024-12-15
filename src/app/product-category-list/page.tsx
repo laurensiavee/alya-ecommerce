685f9d38c38d379b7b252da6a8d8ecfc3e257d17
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-12-15 15:34:24 +0700

    feat: consume delete category

--- src/app/product-category-list/page.tsx
+++ src/app/product-category-list/page.tsx
@@ -3,6 +3,8 @@
 import Card from "@/component/base/Card";
 import Title from "@/component/base/Title";
 import { Category } from "@/entities/product/Category.interface";
+import { DeleteProductCategoryReqParams } from "@/entities/product/DeleteProductCategory.interface";
+import { PatchProductCategoryReqBody, PatchProductCategoryReqParams } from "@/entities/product/PatchProductCategory.interface";
 import { ProductService } from "@/services/product/product.service";
 import { selectToken, setLoading } from "@/store/authSlice";
 import { showToast } from "@/utils/toastNotify";
@@ -31,7 +33,8 @@ const ProductCategoryListPage = () => {
     router.push('/add-product-category')
   };
 
-  const handleDeleteCategory = (event) => {
+  const handleDeleteCategory = (categoryId: string) => {
+    deleteProductCategory(categoryId)
   };
 
   const handleEditCategory = (event) => {
@@ -56,6 +59,29 @@ const ProductCategoryListPage = () => {
     });
   }
 
+  function deleteProductCategory(categoryId: string) {
+    dispatch(setLoading(true))
+
+    const req: DeleteProductCategoryReqParams = {
+        category_id: categoryId,
+    }
+    
+    productService.deleteProductCategory(req, token)
+    .then((resp) => {
+      if(resp.status === 200){
+        showToast(resp.message, "success")
+      }
+      else
+        showToast(resp.message, "error")
+    })
+    .catch((error) => {
+      showToast(error.message, "error")
+      console.error(error.message);
+    }).finally(() => {
+        getProductCategoryList()
+    });
+  }
+
     return (
         <div className='flex justify-center align-center h-[calc(100vh-10rem)] '>
             <Card>
@@ -67,19 +93,21 @@ const ProductCategoryListPage = () => {
                 </div>
                 <div className="mt-5 mx-5">
                   {categoryList.map((category, index) => (
-                    <Card key={index}>
-                        <div className="flex justify-between">
-                            <h1 className="font-bold text-l-text-secondary">{category.category_name}</h1>
-                            <div>
-                                <button onClick={handleDeleteCategory} className="rounded-xl py-2 px-5 ms-2 bg-gradient-to-br from-l-primary to-l-secondary text-d-text font-bold hover:from-l-secondary hover:to-l-primary hover:shadow-2xl hover:shadow-l-primary/50">
-                                    Delete
-                                </button>
-                                <button onClick={handleEditCategory} className="rounded-xl py-2 px-5 ms-2 bg-gradient-to-br from-l-primary to-l-secondary text-d-text font-bold hover:from-l-secondary hover:to-l-primary hover:shadow-2xl hover:shadow-l-primary/50">
-                                    Edit
-                                </button>
+                    <div key={index} className="mt-2">
+                        <Card>
+                            <div className="flex justify-between">
+                                <h1 className="font-bold text-l-text-secondary">{category.category_name}</h1>
+                                <div>
+                                    <button onClick={() => handleDeleteCategory(category.id)} className="rounded-xl py-2 px-5 ms-2 bg-gradient-to-br from-l-primary to-l-secondary text-d-text font-bold hover:from-l-secondary hover:to-l-primary hover:shadow-2xl hover:shadow-l-primary/50">
+                                        Delete
+                                    </button>
+                                    <button onClick={handleEditCategory} className="rounded-xl py-2 px-5 ms-2 bg-gradient-to-br from-l-primary to-l-secondary text-d-text font-bold hover:from-l-secondary hover:to-l-primary hover:shadow-2xl hover:shadow-l-primary/50">
+                                        Edit
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </Card>
+                        </Card>
+                    </div>
                   ))}
                 </div>
             </Card>
