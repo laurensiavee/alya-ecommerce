9d1b4001694d965aee7a3bbb3c161a7cd4307ea5
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-12-27 11:24:38 +0700

    feat: enhance ui navbar

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { selectLoggedIn } from "@/store/authSlice";
 import ProfileTooltip from "../ProfileTooltip";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faPlus, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
 
@@ -25,14 +25,14 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed z-50 top-0 w-full px-9 py-3 bg-gradient-to-r from-l-primary to-l-secondary text-d-text font-medium flex justify-between">
+    <nav className="fixed z-50 top-0 w-full px-9 py-3 bg-gradient-to-r from-l-primary to-l-secondary text-d-text font-medium flex justify-between items-center">
       <div className="flex w-full">
         <button onClick={handleHome} className="me-5">
           Alya
         </button>
         <div className="mx-5 w-1/2 flex">
           <input type="text" placeholder="Search..." className="border-0 text-l-text text-sm rounded-md block w-full p-1.5" />
-          <button className="ms-5 rounded-xl p-1.5 px-5 bg-l-accent text-d-text font-bold hover:shadow-2xl hover:shadow-l-accent/70">
+          <button className="ms-1 rounded-md p-1.5 px-5 border border-text-d-text text-d-text font-bold">
             Filter
           </button>
         </div>
@@ -40,16 +40,18 @@ export default function Navbar() {
       <div>
         {isLoggedIn &&
           <div className="flex gap-7">
-          <FontAwesomeIcon icon={faCartShopping} />
-          <ProfileTooltip />
+            <FontAwesomeIcon icon={faCartShopping} />
+            <ProfileTooltip />
           </div>
         }
         {!isLoggedIn &&
-          <div className="flex gap-5">
-            <button onClick={handleRegister}>
+          <div className="flex gap-7">
+            <button onClick={handleRegister} className="flex gap-2 items-center">
+              <FontAwesomeIcon icon={faPlus} />
               Register
             </button>
-            <button onClick={handleLogin}>
+            <button onClick={handleLogin} className="flex gap-2 items-center">
+              <FontAwesomeIcon icon={faRightToBracket} />
               Login
             </button>
           </div>
